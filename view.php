c96aa276d25ea7ccca3c17d268cc4282801049c2
ahmedddelgabry <66305418+ahmedddelgabry@users.noreply.github.com>
2020-06-11 08:34:39 +0200

    Update view.php

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@123456", "Database" => "ddacdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "apu123456@", "Database" => "ddacdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:ddacs.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
